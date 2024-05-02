3fb833b5d05d25f71b2f1ffd71da992eb74cb2f9
Charlie Hart <charlie@katandtobes.co.uk>
2024-05-02 22:31:03 +0100

    Pre edge_index changes.

--- src/neuralnet/NeuroEvaluate.cpp
+++ src/neuralnet/NeuroEvaluate.cpp
@@ -24,38 +24,42 @@ NNEvaluator::NNEvaluator(std::string model_path):
     NNEvaluator::m_q_weight_to_p = 0.0;
     NNEvaluator::m_product_propagate_weight = 0.0f;
     load_nn_model(model_path);
+    std::cout << "Model loaded." << '\n';
 }
 void NNEvaluator::load_nn_model(std::string model_path){
     this->m_module = torch::jit::load(model_path, torch::kCPU);
 }
 
 void NNEvaluator::generate_adj_matrix(int boardsize) const {
-    Tensor temp_adj_matrix = torch::zeros({boardsize*boardsize, boardsize*boardsize});
+    //if () {
+        Tensor temp_adj_matrix = torch::zeros({boardsize*boardsize, boardsize*boardsize});
 
-    auto is_coord_in_board = [](int x, int y, int boardsize) {
-        return x >= 0 && x < boardsize && y >= 0 && y < boardsize;
-    };
+        auto is_coord_in_board = [](int x, int y, int boardsize) {
+            return x >= 0 && x < boardsize && y >= 0 && y < boardsize;
+        };
 
-    auto index_from_position = [=](int row, int col, int boardsize) {
-        return row * boardsize + col;
-    };
+        auto index_from_position = [=](int row, int col, int boardsize) {
+            return row * boardsize + col;
+        };
 
-    for (int i = 0; i < boardsize; ++i) {
-        for (int j = 0; j < boardsize; ++j) {
-            std::vector<std::pair<int, int>> potential_neighbours = {
+        for (int i = 0; i < boardsize; ++i) {
+            for (int j = 0; j < boardsize; ++j) {
+                std::vector<std::pair<int, int>> potential_neighbours = {
                 {i, j + 1}, {i + 1, j}, {i - 1, j}, {i, j - 1}, {i - 1, j + 1}, {i + 1, j - 1}
             };
-            for (const auto& coord : potential_neighbours) {
-                int x = coord.first;
-                int y = coord.second;
-                if (is_coord_in_board(x, y, boardsize)) {
-                    temp_adj_matrix[index_from_position(i, j, boardsize)][index_from_position(x, y, boardsize)] = 1;
+                for (const auto& coord : potential_neighbours) {
+                    int x = coord.first;
+                    int y = coord.second;
+                    if (is_coord_in_board(x, y, boardsize)) {
+                        temp_adj_matrix[index_from_position(i, j, boardsize)][index_from_position(x, y, boardsize)] = 1;
+                    }
                 }
             }
         }
-    }
-   this->current_adj_matrix_size = boardsize;
-   this->adj_matrix = temp_adj_matrix;
+        this->current_adj_matrix_size = boardsize;
+        this->adj_matrix = temp_adj_matrix;
+    //} else if () {
+    //}
 }
 
 std::vector<torch::jit::IValue>
@@ -112,12 +116,14 @@ NNEvaluator::make_input_tensor(const benzene::bitset_t &black_stones,
  */
 float NNEvaluator::evaluate(const benzene::bitset_t &black, const benzene::bitset_t &white, benzene::HexColor toplay,
                             std::vector<float> &prob_score, std::vector<float> & qValues, int boardsize) const {
-    auto t1=std::chrono::system_clock::now();
+    // auto t1=std::chrono::system_clock::now();
     const std::vector<torch::jit::IValue> inputs=make_input_tensor(black, white, toplay, boardsize);
-
+    // auto t2=std::chrono::system_clock::now();
     Tensor output = m_module.forward(inputs).toTensor()[0];
+    // auto t3=std::chrono::system_clock::now();
     float* p_ret = output.slice(/*dim=*/0, /*start=*/0, /*end=*/121).data_ptr<float>();
     float* q_ret = output.slice(/*dim=*/0, /*start=*/0, /*end=*/121).data_ptr<float>();
+    //std::cout << output.slice(/*dim=*/0, /*start=*/0, /*end=*/121) << '\n';
     float value_estimate = output[-1].item<float>();
     float max_value=-FLT_MAX;
     std::vector<int> empty_points;
@@ -167,13 +173,18 @@ float NNEvaluator::evaluate(const benzene::bitset_t &black, const benzene::bitse
     }
 
     //convert from [-1.0,1.0] to [0.0,1.0]
-    double converted_min_q_value=(-min_q_value+1.0)/2.0f;
-    converted_min_q_value=q_value_with_max_p;
-    double converted_v_value=(1.0+value_estimate)/2.0f;
-    value_estimate=(1.0-m_min_q_combine_weight)*converted_v_value+m_min_q_combine_weight*converted_min_q_value;
-    value_estimate=(1.0-m_product_propagate_weight)*value_estimate + m_product_propagate_weight*(1.0-product_propagate_prob);
+    //double converted_min_q_value=(-min_q_value+1.0)/2.0f;
+    //converted_min_q_value=q_value_with_max_p;
+    //double converted_v_value=(1.0+value_estimate)/2.0f;
+    value_estimate=(1.0+value_estimate)/2.0f;
+    //value_estimate=(1.0-m_min_q_combine_weight)*converted_v_value+m_min_q_combine_weight*converted_min_q_value;
+    //value_estimate=(1.0-m_product_propagate_weight)*value_estimate + m_product_propagate_weight*(1.0-product_propagate_prob);
 
-    auto t2=std::chrono::system_clock::now();
-    //std::cout<<"time cost per eva:"<<std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count()/1e06<<" seconds\n";
+    // auto t4=std::chrono::system_clock::now();
+    // std::cout<<"make adj:"<<std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count()/1e06<<" seconds\n";
+    // std::cout<<"forward :"<<std::chrono::duration_cast<std::chrono::microseconds>(t3-t2).count()/1e06<<" seconds\n";
+    // std::cout<<"the rest:"<<std::chrono::duration_cast<std::chrono::microseconds>(t4-t3).count()/1e06<<" seconds\n";
+    // std::cout<<'\n';
+    // std::cout << value_estimate << '\n';
     return value_estimate;
 }
